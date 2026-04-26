51bdcc01738b09906eb9ee6b177a17b758ad850a
lmbartolome2001 <lmbartolome2001@gmail.com>
2026-04-26 17:56:35 -0300

    refactor improve layout detailscreen

--- app/src/main/java/com/app/cities/presentation/detail/DetailScreen.kt
+++ app/src/main/java/com/app/cities/presentation/detail/DetailScreen.kt
@@ -1,12 +1,22 @@
 package com.app.cities.presentation.detail
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Button
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.app.cities.domain.model.City
 
@@ -16,18 +26,50 @@ fun CityDetailScreen(
     onBack: () -> Unit,
     modifier: Modifier = Modifier
 ) {
-    Column(
-        modifier = modifier
-            .fillMaxSize()
-            .padding(16.dp)
-    ) {
-        Text("${city.name}, ${city.country}")
-        Text("Latitude: ${city.lat}")
-        Text("Longitude: ${city.lon}")
-        Text("Favorite: ${city.isFavorite}")
+    Column(modifier = modifier.fillMaxSize()) {
 
-        Button(onClick = onBack) {
-            Text("Back")
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(8.dp),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            IconButton(onClick = onBack) {
+                Icon(
+                    imageVector = Icons.Default.ArrowBack,
+                    contentDescription = "Back"
+                )
+            }
+
+            Text(
+                text = "City details",
+                style = MaterialTheme.typography.titleLarge
+            )
+        }
+
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(16.dp)
+        ) {
+            Text(
+                text = "${city.name}, ${city.country}",
+                style = MaterialTheme.typography.headlineSmall
+            )
+
+            Spacer(modifier = Modifier.height(16.dp))
+
+            Text("Latitude: ${city.lat}")
+            Text("Longitude: ${city.lon}")
         }
     }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun CityListLoadingPreview() {
+    CityDetailScreen(
+        city = City(id = 1,"Buenos Aires", "AR", -50.2, -68.4, "buenos aires", false),
+        onBack = {  },
+    )
 }
